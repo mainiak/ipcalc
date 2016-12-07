39ac77739a7b6d0ef93e6b5644a7d077098cfa17
Jakub Vitak <jakub@vitak.biz>
2016-12-07 00:15:36 -0800

    Improving IPv4 class
    + Adding binNotation()
    + Added support for integer array in constructor

--- __tests__/ipaddr.test.js
+++ __tests__/ipaddr.test.js
@@ -7,4 +7,10 @@ const IPv4 = require(path.join('..', 'src', 'ipaddr.js'))
 test('Empty constructor', () => {
   let ipAddr = new IPv4()
   expect(ipAddr.dotNotation()).toBe('0.0.0.0')
+  expect(ipAddr.binNotation()).toBe('00000000.00000000.00000000.00000000')
+})
+
+test('Array constructor', () => {
+  let ipAddr = new IPv4([1, 2, 3, 4])
+  expect(ipAddr.dotNotation()).toBe('1.2.3.4')
 })
--- src/ipaddr.js
+++ src/ipaddr.js
@@ -1,15 +1,56 @@
 'use strict'
 
+const BIN_RADIX = 2
 const IPV4_BYTES = 4
+const BYTE_BITS = 8
+
+function pad (binNum, digits) {
+  let i = 0
+  let prefix = ''
+  let zeroCount = digits - binNum.length
+  for (i; i < zeroCount; i++) {
+    prefix += '0'
+  }
+  return (prefix + binNum)
+}
 
 class IPv4 {
 
-  constructor () {
-    this.data = new Uint8Array(IPV4_BYTES)
+  constructor (arg) {
+    this.data = null
+
+    if (arg) {
+      if (typeof arg === 'string') {
+        throw Error('FIX - String arg')
+      }
+
+      if (Array.isArray(arg)) {
+        // TODO: add array checks
+        this.data = Uint8Array.from(arg)
+      }
+
+      if (this.data === null) {
+        throw Error(`Invalid type: ${typeof arg}`)
+      }
+    } else {
+      this.data = new Uint8Array(IPV4_BYTES)
+    }
   }
 
   dotNotation () {
     return `${this.data[0]}.${this.data[1]}.${this.data[2]}.${this.data[3]}`
+  }
+
+  binNotation () {
+    let i
+    let ret = ''
+    for (i = 0; i < IPV4_BYTES; i++) {
+      ret += pad(this.data[i].toString(BIN_RADIX), BYTE_BITS)
+      if (i < (IPV4_BYTES - 1)) {
+        ret += '.'
+      }
+    }
+    return ret
   }
 }
 
