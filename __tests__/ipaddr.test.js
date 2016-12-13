ec48ab457f20c7baf59fba1ccf25889c9d33fdac
Jakub Vitak <jakub@vitak.biz>
2016-12-13 13:52:59 +0100

    Extending IPv4 class
    + Added clone constructor - IPv4 instance as argument
    + Added string netmask constructor '/N' ie '/8'
    + Added instance method isInRange(ipAddr1, ipAddr2)
    + Added instance method isNetmask()
    + Added instance method binInvert()
    + Added instance method binOr()
    + Added instance method binAnd()
    + Added static method IPv4.info(ipAddr, netmask)
    + Added static method IPv4.compare(ipAddr1, ipAddr2)

--- __tests__/ipaddr.test.js
+++ __tests__/ipaddr.test.js
@@ -14,3 +14,243 @@ test('Array constructor', () => {
   let ipAddr = new IPv4([1, 2, 3, 4])
   expect(ipAddr.dotNotation()).toBe('1.2.3.4')
 })
+
+test('Invalid constructor', () => {
+  expect(() => {
+    return new IPv4(1234)
+  }).toThrowError('Invalid type: number')
+})
+
+test('Invalid string constructor', () => {
+  expect(() => {
+    return new IPv4('bla bla')
+  }).toThrowError('FIX - String arg') // FIXME
+})
+
+test('Clone constructor', () => {
+  let ipAddr1 = new IPv4([1, 2, 3, 4])
+  let ipAddr2 = new IPv4(ipAddr1)
+  expect(ipAddr1).not.toBe(ipAddr2)
+  expect(ipAddr1.dotNotation()).toBe(ipAddr2.dotNotation())
+})
+
+/*
+test('String constructor', () => {
+  let ipAddr = new IPv4('1.2.3.4')
+  expect(ipAddr.binNotation()).toBe('11111111.11110000.00000000.00000000')
+})
+*/
+
+test('String netmask constructor - bellow lower range', () => {
+  expect(() => {
+    return new IPv4('/0')
+  }).toThrowError('Invalid netmask /0')
+})
+
+test('String netmask constructor - above higher range', () => {
+  expect(() => {
+    return new IPv4('/32')
+  }).toThrowError('Invalid netmask /32')
+})
+
+test('String netmask constructor - lower border case 1', () => {
+  let ipAddr = new IPv4('/1')
+  expect(ipAddr.binNotation()).toBe('10000000.00000000.00000000.00000000')
+})
+
+test('String netmask constructor - lower border case 2', () => {
+  let ipAddr = new IPv4('/2')
+  expect(ipAddr.binNotation()).toBe('11000000.00000000.00000000.00000000')
+})
+
+test('String netmask constructor', () => {
+  let ipAddr = new IPv4('/12')
+  expect(ipAddr.binNotation()).toBe('11111111.11110000.00000000.00000000')
+})
+
+test('String netmask constructor - higher border case', () => {
+  let ipAddr = new IPv4('/31')
+  expect(ipAddr.binNotation()).toBe('11111111.11111111.11111111.11111110')
+})
+
+test('isNetmask() - nope', () => {
+  let ipAddr = new IPv4([192, 168, 1, 2])
+  expect(ipAddr.isNetmask()).toBe(false)
+})
+
+test('isNetmask() /8', () => {
+  let ipAddr = new IPv4([255, 0, 0, 0])
+  expect(ipAddr.isNetmask()).toBe(true)
+})
+
+test('isNetmask() /16', () => {
+  let ipAddr = new IPv4([255, 255, 0, 0])
+  expect(ipAddr.isNetmask()).toBe(true)
+})
+
+test('isNetmask() /24', () => {
+  let ipAddr = new IPv4([255, 255, 255, 0])
+  expect(ipAddr.isNetmask()).toBe(true)
+})
+
+test('IPv4.compare() - Invalid 1st argument', () => {
+  expect(() => {
+    IPv4.compare()
+  }).toThrowError('First argument is not IPv4 object')
+})
+
+test('IPv4.compare() - Invalid 2nd argument', () => {
+  let ipAddr1 = new IPv4()
+  expect(() => {
+    IPv4.compare(ipAddr1)
+  }).toThrowError('Second argument is not IPv4 object')
+})
+
+test('IPv4.compare() - equal', () => {
+  let ipAddr1 = new IPv4()
+  let ipAddr2 = new IPv4()
+  expect(IPv4.compare(ipAddr1, ipAddr2)).toBe(0)
+})
+
+test('IPv4.compare() - octet 1 higher', () => {
+  let ipAddr1 = new IPv4([1, 0, 0, 0])
+  let ipAddr2 = new IPv4([0, 0, 0, 0])
+  expect(IPv4.compare(ipAddr1, ipAddr2)).toBe(1)
+})
+
+test('IPv4.compare() - octet 1 lower', () => {
+  let ipAddr1 = new IPv4([0, 0, 0, 0])
+  let ipAddr2 = new IPv4([1, 0, 0, 0])
+  expect(IPv4.compare(ipAddr1, ipAddr2)).toBe(-1)
+})
+
+test('IPv4.compare() - octet 2 higher', () => {
+  let ipAddr1 = new IPv4([0, 1, 0, 0])
+  let ipAddr2 = new IPv4([0, 0, 0, 0])
+  expect(IPv4.compare(ipAddr1, ipAddr2)).toBe(1)
+})
+
+test('IPv4.compare() - octet 2 lower', () => {
+  let ipAddr1 = new IPv4([0, 0, 0, 0])
+  let ipAddr2 = new IPv4([0, 1, 0, 0])
+  expect(IPv4.compare(ipAddr1, ipAddr2)).toBe(-1)
+})
+
+test('IPv4.compare() - octet 3 higher', () => {
+  let ipAddr1 = new IPv4([0, 0, 1, 0])
+  let ipAddr2 = new IPv4([0, 0, 0, 0])
+  expect(IPv4.compare(ipAddr1, ipAddr2)).toBe(1)
+})
+
+test('IPv4.compare() - octet 3 lower', () => {
+  let ipAddr1 = new IPv4([0, 0, 0, 0])
+  let ipAddr2 = new IPv4([0, 0, 1, 0])
+  expect(IPv4.compare(ipAddr1, ipAddr2)).toBe(-1)
+})
+
+test('IPv4.compare() - octet 4 higher', () => {
+  let ipAddr1 = new IPv4([0, 0, 0, 1])
+  let ipAddr2 = new IPv4([0, 0, 0, 0])
+  expect(IPv4.compare(ipAddr1, ipAddr2)).toBe(1)
+})
+
+test('IPv4.compare() - octet 4 lower', () => {
+  let ipAddr1 = new IPv4([0, 0, 0, 0])
+  let ipAddr2 = new IPv4([0, 0, 0, 1])
+  expect(IPv4.compare(ipAddr1, ipAddr2)).toBe(-1)
+})
+
+test('isInRange() - Invalid 1st argument', () => {
+  let ipAddr = new IPv4([192, 168, 1, 2])
+  expect(() => {
+    ipAddr.isInRange()
+  }).toThrowError('First argument is not IPv4 object')
+})
+
+test('isInRange() - Invalid 2nd argument', () => {
+  let ipAddr = new IPv4([192, 168, 1, 2])
+  let ipAddrLow = new IPv4([192, 168, 0, 0])
+  expect(() => {
+    ipAddr.isInRange(ipAddrLow)
+  }).toThrowError('Second argument is not IPv4 object')
+})
+
+test('isInRange() - lower border', () => {
+  let ipAddrLow = new IPv4([192, 168, 0, 0])
+  let ipAddrHigh = new IPv4([192, 168, 255, 255])
+  expect(ipAddrLow.isInRange(ipAddrLow, ipAddrHigh)).toBe(true)
+})
+
+test('isInRange() - higher border', () => {
+  let ipAddrLow = new IPv4([192, 168, 0, 0])
+  let ipAddrHigh = new IPv4([192, 168, 255, 255])
+  expect(ipAddrHigh.isInRange(ipAddrLow, ipAddrHigh)).toBe(true)
+})
+
+test('isInRange() - in range', () => {
+  let ipAddr = new IPv4([192, 168, 1, 2])
+  let ipAddrLow = new IPv4([192, 168, 0, 0])
+  let ipAddrHigh = new IPv4([192, 168, 255, 255])
+  expect(ipAddr.isInRange(ipAddrLow, ipAddrHigh)).toBe(true)
+})
+
+test('isInRange() - in range & swap order', () => {
+  let ipAddr = new IPv4([192, 168, 1, 2])
+  let ipAddrLow = new IPv4([192, 168, 0, 0])
+  let ipAddrHigh = new IPv4([192, 168, 255, 255])
+  expect(ipAddr.isInRange(ipAddrHigh, ipAddrLow)).toBe(true)
+})
+
+test('isInRange() - bellow range', () => {
+  let ipAddr = new IPv4([10, 10, 1, 2])
+  let ipAddrLow = new IPv4([192, 168, 0, 0])
+  let ipAddrHigh = new IPv4([192, 168, 255, 255])
+  expect(ipAddr.isInRange(ipAddrLow, ipAddrHigh)).toBe(false)
+})
+
+test('isInRange() - above range', () => {
+  let ipAddr = new IPv4([200, 200, 1, 2])
+  let ipAddrLow = new IPv4([192, 168, 0, 0])
+  let ipAddrHigh = new IPv4([192, 168, 255, 255])
+  expect(ipAddr.isInRange(ipAddrLow, ipAddrHigh)).toBe(false)
+})
+
+test('IPv4.info() - invalid 1st argument', () => {
+  expect(() => {
+    IPv4.info()
+  }).toThrowError('First argument is not IPv4 object')
+})
+
+test('IPv4.info() - invalid 2nd argument', () => {
+  let ipAddr1 = new IPv4([1, 2, 3, 4])
+  expect(() => {
+    IPv4.info(ipAddr1)
+  }).toThrowError('Second argument is not IPv4 object')
+})
+
+test('IPv4.info() - invalid 2nd argument', () => {
+  let ipAddr1 = new IPv4([1, 2, 3, 4])
+  let ipAddr2 = new IPv4([1, 2, 3, 4])
+  expect(() => {
+    IPv4.info(ipAddr1, ipAddr2)
+  }).toThrowError('Second argument is not a netmask')
+})
+
+test('IPv4.info()', () => {
+  let ipAddr = new IPv4([172, 18, 3, 4])
+  let netmask = new IPv4('/12')
+  let infoObj = IPv4.info(ipAddr, netmask)
+  expect(typeof infoObj).toBe('object')
+
+  expect(infoObj.ipAddr instanceof IPv4).toBe(true)
+  expect(infoObj.netmask instanceof IPv4).toBe(true)
+  expect(infoObj._invertedNetmask instanceof IPv4).toBe(true)
+  expect(infoObj.networkIP instanceof IPv4).toBe(true)
+  expect(infoObj.broadcast instanceof IPv4).toBe(true)
+
+  expect(infoObj.ipAddr).not.toBe(ipAddr)
+  expect(infoObj.netmask).not.toBe(netmask)
+
+  expect(infoObj.networkIP.dotNotation()).toBe('172.16.0.0')
+  expect(infoObj.broadcast.dotNotation()).toBe('172.31.255.255')
+})
--- src/ipaddr.js
+++ src/ipaddr.js
@@ -1,8 +1,10 @@
 'use strict'
 
 const BIN_RADIX = 2
+const DEC_RADIX = 10
 const IPV4_BYTES = 4
 const BYTE_BITS = 8
+const NETMASK_R = /\/(\d{1,2})/
 
 function pad (binNum, digits) {
   let i = 0
@@ -20,8 +22,69 @@ class IPv4 {
     this.data = null
 
     if (arg) {
+      // clone constructor
+      if (arg instanceof IPv4) {
+        this.data = arg.data.slice(0)
+      }
+
       if (typeof arg === 'string') {
-        throw Error('FIX - String arg')
+        if (NETMASK_R.test(arg)) {
+          let r = NETMASK_R.exec(arg)
+          let num = parseInt(r[1], DEC_RADIX)
+
+          this.data = new Uint8Array(IPV4_BYTES)
+          this.data[0] = 128
+
+          if ((num >= 1) && (num <= 31)) {
+            let i = 0
+            let max = num - 1
+            for (i; i < max; i++) {
+              // test bit shift between octets first
+
+              let shiftOctet12 = false
+              if ((this.data[0] & 1) === 1) {
+                shiftOctet12 = true
+              }
+
+              let shiftOctet23 = false
+              if ((this.data[1] & 1) === 1) {
+                shiftOctet23 = true
+              }
+
+              let shiftOctet34 = false
+              if ((this.data[2] & 1) === 1) {
+                shiftOctet34 = true
+              }
+
+              // shift all octets
+              this.data[0] = this.data[0] >>> 1
+              this.data[1] = this.data[1] >>> 1
+              this.data[2] = this.data[2] >>> 1
+              this.data[3] = this.data[3] >>> 1
+
+              this.data[0] |= 128
+
+              // shift bits betweens octets if needed
+              if (shiftOctet12) {
+                this.data[1] |= 128
+              }
+
+              if (shiftOctet23) {
+                this.data[2] |= 128
+              }
+
+              if (shiftOctet34) {
+                this.data[3] |= 128
+              }
+            }
+          } else {
+            throw new Error(`Invalid netmask /${num}`)
+          }
+        }
+
+        if (this.data === null) {
+          throw Error('FIX - String arg')
+        }
       }
 
       if (Array.isArray(arg)) {
@@ -51,6 +114,157 @@ class IPv4 {
       }
     }
     return ret
+  }
+
+  isNetmask () {
+    let bits = this.binNotation().replace(/\./g, '')
+    return /^1+0+$/.test(bits)
+  }
+
+  isPrivateIP () {
+    // in range
+  }
+
+  isIPv4LL () {
+  }
+
+  isInRange (ipAddr1, ipAddr2) {
+    if (ipAddr1 instanceof IPv4 === false) {
+      throw new Error('First argument is not IPv4 object')
+    }
+
+    if (ipAddr2 instanceof IPv4 === false) {
+      throw new Error('Second argument is not IPv4 object')
+    }
+
+    let range = [ipAddr1, ipAddr2]
+    range.sort(IPv4.compare)
+    let ipAddrLow = range[0]
+    let ipAddrHigh = range[1]
+
+    if (IPv4.compare(this, ipAddrLow) === -1) {
+      return false
+    }
+
+    if (IPv4.compare(this, ipAddrHigh) === 1) {
+      return false
+    }
+
+    return true
+  }
+
+  binInvert () {
+    let ipAddr = new IPv4(this)
+    ipAddr.data[0] = ~ipAddr.data[0]
+    ipAddr.data[1] = ~ipAddr.data[1]
+    ipAddr.data[2] = ~ipAddr.data[2]
+    ipAddr.data[3] = ~ipAddr.data[3]
+    return ipAddr
+  }
+
+  binOr (ipAddr2) {
+    let ipAddr1 = new IPv4(this)
+    ipAddr1.data[0] |= ipAddr2.data[0]
+    ipAddr1.data[1] |= ipAddr2.data[1]
+    ipAddr1.data[2] |= ipAddr2.data[2]
+    ipAddr1.data[3] |= ipAddr2.data[3]
+    return ipAddr1
+  }
+
+  binAnd (ipAddr2) {
+    let ipAddr1 = new IPv4(this)
+    ipAddr1.data[0] &= ipAddr2.data[0]
+    ipAddr1.data[1] &= ipAddr2.data[1]
+    ipAddr1.data[2] &= ipAddr2.data[2]
+    ipAddr1.data[3] &= ipAddr2.data[3]
+    return ipAddr1
+  }
+
+  static compare (ipAddr1, ipAddr2) {
+    if (ipAddr1 instanceof IPv4 === false) {
+      throw new Error('First argument is not IPv4 object')
+    }
+
+    if (ipAddr2 instanceof IPv4 === false) {
+      throw new Error('Second argument is not IPv4 object')
+    }
+
+    // octect1 higher or lower?
+    if (ipAddr1.data[0] !== ipAddr2.data[0]) {
+      if (ipAddr1.data[0] > ipAddr2.data[0]) {
+        // console.log(`o1: 1 // ${ipAddr1.data[0]} > ${ipAddr2.data[0]}`) // XXX
+        return 1
+      } else {
+        // console.log(`o1: -1 // ${ipAddr1.data[0]} > ${ipAddr2.data[0]}`) // XXX
+        return -1
+      }
+    }
+
+    // octect2 higher or lower?
+    if (ipAddr1.data[1] !== ipAddr2.data[1]) {
+      if (ipAddr1.data[1] > ipAddr2.data[1]) {
+        // console.log(`o2: 1 // ${ipAddr1.data[1]} > ${ipAddr2.data[1]}`) // XXX
+        return 1
+      } else {
+        // console.log(`o2: -1 // ${ipAddr1.data[1]} > ${ipAddr2.data[1]}`) // XXX
+        return -1
+      }
+    }
+
+    // octect3 higher or lower?
+    if (ipAddr1.data[2] !== ipAddr2.data[2]) {
+      if (ipAddr1.data[2] > ipAddr2.data[2]) {
+        // console.log(`o3: 1 // ${ipAddr1.data[2]} > ${ipAddr2.data[2]}`) // XXX
+        return 1
+      } else {
+        // console.log(`o3: -1 // ${ipAddr1.data[2]} > ${ipAddr2.data[2]}`) // XXX
+        return -1
+      }
+    }
+
+    // octect4 higher or lower?
+    if (ipAddr1.data[3] !== ipAddr2.data[3]) {
+      if (ipAddr1.data[3] > ipAddr2.data[3]) {
+        // console.log(`o4: 1 // ${ipAddr1.data[3]} > ${ipAddr2.data[3]}`) // XXX
+        return 1
+      } else {
+        // console.log(`o4: -1 // ${ipAddr1.data[3]} > ${ipAddr2.data[3]}`) // XXX
+        return -1
+      }
+    }
+
+    // console.log('same octets') // XXX
+    return 0
+  }
+
+  static info (ipAddr, netmask) {
+    if (ipAddr instanceof IPv4 === false) {
+      throw new Error('First argument is not IPv4 object')
+    }
+
+    if (netmask instanceof IPv4 === false) {
+      throw new Error('Second argument is not IPv4 object')
+    }
+
+    if (!netmask.isNetmask()) {
+      throw new Error('Second argument is not a netmask')
+    }
+
+    let invertedNetmask = netmask.binInvert()
+    let networkIP = ipAddr.binAnd(netmask)
+
+    return {
+      // include copies
+      ipAddr: new IPv4(ipAddr),
+      netmask: new IPv4(netmask),
+
+      // add
+      _invertedNetmask: invertedNetmask,
+      networkIP,
+
+      // compute
+      broadcast: networkIP.binOr(invertedNetmask)
+    }
   }
 }
 
